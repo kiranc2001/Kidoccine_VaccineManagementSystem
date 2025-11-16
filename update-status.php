f75cdb142511d7896a375e05274e386a29333730
Kiran C <kirangowda0212@gmail.com>
2025-11-16 22:34:00 +0530

    Update update-status.php

--- update-status.php
+++ update-status.php
@@ -63,13 +63,13 @@ $email = isset($_POST['email']) ? $_POST['email'] : '';
                     $mail->isSMTP();
                    $mail->Host = 'smtp.gmail.com'; // Specify your SMTP server
     $mail->SMTPAuth = true;
-    $mail->Username = 'vaccine.govt@gmail.com'; // SMTP username
-    $mail->Password = 'qnaqgmrnamvsuzis'; // SMTP password
+    $mail->Username = 'kirangowda0212@gmail.com'; // SMTP username
+    $mail->Password = 'llhrxpyvxrxzlssr'; // SMTP password
     $mail->SMTPSecure = 'tls'; // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 587; // TCP port to connect t port to connect to
 
         // Recipients
-        $mail->setFrom('vaccine.govt@gmail.com','Kidoccine');
+        $mail->setFrom('kirangowda0212@gmail.com','Kidoccine');
         $mail->addAddress($parent_email);
                     $mail->isHTML(true);
                     $mail->Subject = 'Vaccine Status Update';
@@ -169,3 +169,4 @@ echo "<script>alert('Status Updated'); window.location.href = 'hospitalget_paren
 
 $conn->close();
 ?>
+
